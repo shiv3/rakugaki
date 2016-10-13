f24b559019134478b60cfc5f21d295e5b355aae6
avis <b3326@oic.jp>
2016-10-13 19:22:07 +0900

    tes

--- docs/js/timetable/table.js
+++ docs/js/timetable/table.js
@@ -33,6 +33,7 @@ console.log("次の" + vehicle + "は" + new Date(recenttime[0]));
 let next = recenttime[0];
 if(next){
     console.log("次の" + vehicle + "まであと " + new Date( recenttime[0] - now ).getMinutes() + " 分です");
+    document.write("次の" + vehicle + "まであと " + new Date( recenttime[0] - now ).getMinutes() + " 分です");
     if(recenttime[1]){
         console.log("その次の" + vehicle + "まであと " + new Date( recenttime[1] - now ).getMinutes() + " 分です");
     }else{
